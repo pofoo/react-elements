5312ef57ec5e9f7387c29dcbefc57cc494439a4e
Charlie <lucharles3@gmail.com>
2021-10-24 18:27:13 -0700

    SVG

--- elements/index.ts
+++ elements/index.ts
@@ -1,9 +1,11 @@
 import ProgressBar from './progress-bar';
 import { Button, AccessibleButton, LinkButton } from './button';
+import SVG from './svg';
 
 export {
     ProgressBar,
     Button,
     AccessibleButton,
     LinkButton,
+    SVG,
 }
---
+++ elements/svg/SVG.tsx
@@ -0,0 +1,31 @@
+
+/* TYPES */
+interface Props {
+    className?: string;
+    data: string;
+    alt: string;
+    width: number | string;
+    height: number | string;
+    type: 'image/svg+xml';
+};
+
+const SVG = ( {
+    className,
+    data, // path to SVG
+    alt,
+    width,
+    height,
+    type='image/svg+xml',
+}: Props ) => {
+
+    return (
+        <object className={className}
+            width={width} height={height}
+            data={data}
+            type={type}>
+        {alt}
+        </object>
+    );
+}
+
+export default SVG;
