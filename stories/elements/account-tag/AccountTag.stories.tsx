ea4700d321410a75f678d757c2ab57494a7f9ddd
Charlie <lucharles3@gmail.com>
2021-11-15 19:48:04 -0800

    image

--- stories/elements/account-tag/AccountTag.stories.tsx
+++ stories/elements/account-tag/AccountTag.stories.tsx
@@ -15,7 +15,7 @@ AccountTag.args = {
     content: {
         name: 'Charlie Lu',
         image: {
-          src: '/static/images/goat.jpg',
+          src: 'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2776&q=80',
           alt: 'Account Image',
         }
     }
