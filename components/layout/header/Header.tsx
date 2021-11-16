a49d2b1d870eab1a49f1351e1ce27434c546903a
Charlie <lucharles3@gmail.com>
2021-11-15 22:57:44 -0800

    checkpoint - header bug

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -1,4 +1,5 @@
 // dependencies
+import { useState, useEffect } from 'react';
 import { FC } from 'react';
 
 /* TYPES */
@@ -6,15 +7,44 @@ interface Props {
     className?: string;
 }
 
+/**
+ * Header that hides on scroll down and shows on scroll up.
+ */
 const Header: FC<Props> = ( {
     children,
     className,
 } ) => {
     
+    /* HOOKS */
+    const [ scrollPos, setScrollPos ] = useState<number>( 0 );
+    const [ scrollDirection, setScrollDirection ] = useState<'up' | 'down' | null>( null );
+
     const headerClasses = `
         header-wrapper
         ${className}
+        ${scrollDirection === 'down' ? 'hide' : ''}
     `;
+
+    // TO-DO - fix this
+    /* FUNCTIONS */
+    const handleScroll = ( prevScrollPos: number ): void => {
+        const currScrollPos = window.scrollY;
+        console.log( `Current: ${currScrollPos}, Prev: ${prevScrollPos}` );
+        const scrollDirection = currScrollPos >= prevScrollPos ? 'down' : 'up';
+        
+        setScrollPos( currScrollPos );
+        setScrollDirection( scrollDirection );
+    }
+
+    // TO-DO - there should be a more efficient way to add event listener
+    // right now it renders on the smallest scroll amount
+    // the scroll amount can be bigger
+    useEffect( () => {
+        window.addEventListener( 'scroll', () => handleScroll( scrollPos ) );
+        return () => {
+            window.removeEventListener( 'scroll', () => handleScroll( scrollPos ) );
+        }
+    }, [ scrollPos ] );
 
     return (
         <header className={headerClasses}>
