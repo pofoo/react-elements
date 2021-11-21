8ab01abd26d9c79b961612611c24e5c1db3b2bf6
Charlie <lucharles3@gmail.com>
2021-11-21 14:06:05 -0800

    Update objectAtLeastOne.d.ts
    clearer generic

--- types/objectAtLeastOne.d.ts
+++ types/objectAtLeastOne.d.ts
@@ -1,5 +1,5 @@
 // must contatin AT LEAST one of the key value pairs - specified as a generic when declaring the type
-type ObjectAtLeastOne<T, Keys extends keyof T = keyof T> = Partial<T> & { 
+type ObjectAtLeastOne<T extends object, Keys extends keyof T = keyof T> = Partial<T> & { 
     [K in Keys]: Required<Pick<T, K>> 
 }[Keys];
 
