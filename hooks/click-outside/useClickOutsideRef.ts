0b28dcdc0ac63cac04bfd8587782b009729f9763
Charlie <lucharles3@gmail.com>
2021-11-24 09:42:11 -0800

    config + clickoutside

--- hooks/click-outside/useClickOutsideRef.ts
+++ hooks/click-outside/useClickOutsideRef.ts
@@ -1,14 +1,20 @@
 // dependencies
 import { useEffect, useRef, RefObject } from 'react';
 
+/* TYPES */
+type Options = {
+    enableEscape?: boolean;
+}
+
 /**
  * Returns refs that calls an onClick function everytime a click is made outside of that HTML Element
  * Defaults to 1 ref returned - all refs are returned in an array
  * numRefs only takes positive numbers
  */
 const useClickOutsideRef = <T extends HTMLElement>( 
-    onClick: ( event: PointerEvent ) => void,
-    numRefs: number=1 // this should only be a positive number
+    onClick: () => void,
+    options: Options={},
+    numRefs: number=1, // this should only be a positive number
     // TO-DO - the generic T right now assumes that all the refs are going to be the same kind of HTMLElement
 ): RefObject<T>[] => {
 
@@ -32,15 +38,34 @@ const useClickOutsideRef = <T extends HTMLElement>(
                 if ( ref.current.contains( event.target as T ) ) isClicked = true;
             }
             // the HTMLElement was not clicked - call the onClick function
-            if ( !isClicked ) onClick( event );
-        }   
+            if ( !isClicked ) onClick();
+        }
+
         // attaching the clickOutsideFn to all the refs in the refList
         useEffect( () => {
             document.addEventListener( 'pointerdown', clickOutsideFn );
             return () => {
                 document.removeEventListener( 'pointerdown', clickOutsideFn );
             }
-        }, [] );  
+        }, [] );
+
+        // options
+        const { enableEscape=true } = options;
+        // call onClick when user presses the escape key
+        if ( enableEscape ) {
+            const handleEscape = ( event: KeyboardEvent ) => {
+                if ( event.key === 'Escape' ) {
+                    onClick();
+                }
+            }
+            // attaching the clickOutsideFn to the escape key
+            useEffect( () => {
+                document.addEventListener( 'keydown', handleEscape );
+                return () => {
+                    document.removeEventListener( 'keydown', handleEscape );
+                }
+            }, [] );
+        }
 
         return refList;
     } 
