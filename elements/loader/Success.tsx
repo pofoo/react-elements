b853e81f0cdef0b8d1db34f93995c53fe5cdddd9
Charlie <lucharles3@gmail.com>
2021-12-03 19:08:04 -0800

    checkpoint
    simplification of success animation

--- elements/loader/Success.tsx
+++ elements/loader/Success.tsx
@@ -9,14 +9,11 @@ const Success = ( {
 }: Props ) => {
 
     return (
-        <div className='success-wrapper' aria-label={ariaLabel} role='presentation'>
-            <span className='check-icon'>
-                <span className='icon-line line-tip' role='presentation' />
-                <span className='icon-line line-tip' role='presentation' />
-                <span className='icon-circle' role='presenation' />
-                <span className='icon-fix' role='presentation' />
-            </span>
-        </div>
+        <span className='success-wrapper' aria-label={ariaLabel} role='presentation'>
+            <span className='check-line line-tip' role='presentation' />
+            <span className='check-line line-long' role='presentation' />
+            <span className='circle' role='presenation' />
+        </span>
     )
 }
 
